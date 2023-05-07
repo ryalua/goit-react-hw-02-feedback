4056f73d4bcd7633123ff917f7d21bd0300dbfdb
Alex <alexrybalka65@gmail.com>
2023-05-07 19:34:35 +0300

    чистит код

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -62,28 +62,3 @@ export class App extends React.Component {
     );
   }
 }
-
-// const App = () => (
-//   <>
-//     <Feedback />
-//   </>
-// );
-
-// export default App;
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       <Feedback/>
-//     </div>
-//   );
-// };
